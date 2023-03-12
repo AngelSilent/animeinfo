f5b4e634747f9b8f5235368ba4f1ed7f55d0e794
AngelSilent <angelstarkpana@gmail.com>
2023-03-12 14:36:13 -0700

    Consulta de información

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ mysql = MySQL(app)
 
 @app.route("/")
 def main():
-    return jsonify({"title": "animeinfo"})
+    return jsonify({"title": "animeinfo API"})
 
-@app.route("/search",methods=['GET'])
+@app.route("/anime", methods=['GET'])
 def search():
-    try :
-       idAnime = int(request.args["anime-id"]);
+    try:
+        idAnime = int(request.args["anime-id"])
     except:
         idAnime = False
     if isinstance(idAnime, int):
@@ -32,22 +32,32 @@ def search():
             "id": dataAnime[0],
             "title": dataAnime[1],
             "description": dataAnime[2],
-            "year": dataAnime[3]
+            "year": dataAnime[3],
+            "genre": dataAnime[4],
         }
         return jsonify(anime)
     else:
-        return jsonify({"error": "Ocurrio un error"});
+        return jsonify({"error": "Ocurrió un error"})
 
 
-@app.route("/all", methods=["GET"])
+@app.route("/anime/all", methods=["GET"])
 def allAnimes():
     cur = mysql.connection.cursor()
     cur.execute("""SELECT * FROM animes""")
-    allDatas = cur.fetchmany()
-    return jsonify(allDatas)
+    allData = cur.fetchall()
+    allAnimes = []
+    for anime in allData:
+        allAnimes.append({
+            "id": anime[0],
+            "title": anime[1],
+            "description": anime[2],
+            "year": anime[3],
+            "genre": anime[4],
+        })
+    return jsonify(allAnimes)
+
 
 @app.route("/about")
 def anime():
     with open("aboutl.json", "r") as data:
-        about = json.load(data)
-    return about;
+        return jsonify(json.load(data))
